53b483ad59d514cade1e41c372fd1d5152ae1ce0
aakashtuteja4 <aakashtuteja4@gmail.com>
2022-03-14 23:56:34 +0530

    added new code for two string problem

---
+++ python/twoStrings.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'twoStrings' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s1
+#  2. STRING s2
+#
+
+# s1 = 'hello'
+
+# s2 = 'world'
+
+# set(s2).intersection(set(s1))
+
+def twoStrings(s1, s2):
+    # Write your code here
+    common = set(s2).intersection(set(s1))
+    if len(common) >= 1:
+        return 'YES'
+    else:
+        return 'NO'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
